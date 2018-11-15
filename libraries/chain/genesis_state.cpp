e06bdc5a392a2f1556d98a4702db1b68cabf9f81
bookcool <sunshuhan666@163.com>
2018-11-15 19:22:15 +0800

    update chain config and update contract block num config

--- libraries/chain/genesis_state.cpp
+++ libraries/chain/genesis_state.cpp
@@ -16,7 +16,7 @@ genesis_state::genesis_state() {
 }
 
 const fc::sha256 old_chain_id = fc::sha256("bd61ae3a031e8ef2f97ee3b0e62776d6d30d4833c8f7c1645c657b149151004b");
-const fc::sha256 new_chain_id = fc::sha256("78215edcce90572883cf4ed9a6763d94e9c0d566f709141647709091c7d1378b");
+const fc::sha256 new_chain_id = fc::sha256("a877ed4d24f9f60f0c78536351a99f37e0b203038878a48b9ea8599ab312be45");
 
 chain::chain_id_type genesis_state::compute_chain_id() const {
    digest_type::encoder enc;

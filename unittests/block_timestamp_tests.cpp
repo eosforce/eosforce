6279831659396082c6f03e4d63c95dc997f75fce
yinhp <yzuyhp@sina.com>
2018-12-05 17:11:53 +0800

    fix time conversion errors

--- unittests/block_timestamp_tests.cpp
+++ unittests/block_timestamp_tests.cpp
@@ -22,7 +22,7 @@ BOOST_AUTO_TEST_CASE(constructor_test) {
         
 	fc::time_point t(fc::seconds(978307200));	
 	block_timestamp_type bt2(t);
-	BOOST_TEST( bt2.slot == (978307200 - 946684800)*2, "Time point constructor gives wrong value");
+	BOOST_TEST( bt2.slot == (978307200 - 946684800)/3/**2*/, "Time point constructor gives wrong value");
 }
 
 BOOST_AUTO_TEST_CASE(conversion_test) {
@@ -32,7 +32,7 @@ BOOST_AUTO_TEST_CASE(conversion_test) {
 
 	block_timestamp_type bt1(200);
 	t = (fc::time_point)bt1;
-	BOOST_TEST(t.time_since_epoch().to_seconds() == 946684900ll, "Time point conversion failed");
+	BOOST_TEST(t.time_since_epoch().to_seconds() == 946685400ll/*946684900ll*/, "Time point conversion failed");
 
 }
 

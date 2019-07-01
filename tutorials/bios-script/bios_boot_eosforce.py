27a1b87216bb14ccb3cc92f7af9aedbc20d25806
fanyang <fanyang.coder@gmail.com>
2019-07-01 16:02:34 +0800

    update boot script to allow misg

--- tutorials/bios-script/bios_boot_eosforce.py
+++ tutorials/bios-script/bios_boot_eosforce.py
@@ -196,8 +196,13 @@ def setFee(account, act, fee, cpu, net, ram):
 def stepSetFuncs():
     # we need set some func start block num
     setFee('eosio', 'setconfig', '0.0100', 100000, 1000000, 1000)
-    setFuncStartBlock('f.ram4vote', 10)
-    setFuncStartBlock('f.onfeeact', 15)
+    setFuncStartBlock('f.system1', 6)
+    setFuncStartBlock('f.msig', 8)
+    setFuncStartBlock('f.prods', 10)
+    setFuncStartBlock('f.eosio', 12)
+    setFuncStartBlock('f.feelimit', 14)
+    setFuncStartBlock('f.ram4vote', 16)
+    setFuncStartBlock('f.onfeeact', 18)
 
 def clearData():
     stepKillAll()

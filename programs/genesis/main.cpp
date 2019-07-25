663774ada26a975720a005647ffa8e6731eed2e7
fanyang <fanyang.coder@gmail.com>
2019-07-25 22:17:24 +0800

    format genesis code and add more account to test

--- programs/genesis/main.cpp
+++ programs/genesis/main.cpp
@@ -45,59 +45,65 @@ int main( int argc, const char **argv ) {
    // EOS7R82SaGaJubv23GwXHyKT4qDCVXi66qkQrnjwmBUvdA4dyzEPG
    const auto bpsign_private_key = fc::crypto::private_key(std::string("5JfjatHRwbmY8SfptFRxHnYUctfnuaxANTGDYUtkfrrBDgkh3hB"));
 
+   const auto& biosbp_str = std::string("biosbp");
+
    for( int i = 0; i < config::max_producers; i++ ) {
-      auto key = bp_private_key;
-      auto pub_key = key.get_public_key();
-      eosio::chain::account_tuple tu;
-      tu.key = pub_key;
-      tu.asset = eosio::chain::asset(100000000);
-      std::string name("biosbp");
-      char mark = 'a' + i;
-      name.append(1u, mark);
-      tu.name = string_to_name(name.c_str());
+      const auto key = bp_private_key;
+      const auto pub_key = key.get_public_key();
+
+      const auto name_str = biosbp_str + static_cast<char>('a' + i);
+      const auto bpname = string_to_name( name_str.c_str() );
+
+      auto tu = eosio::chain::account_tuple {
+         pub_key,
+         eosio::chain::asset(1000 * 10000),
+         bpname
+      };
+
       gs.initial_account_list.push_back(tu);
-      auto sig_key = bpsign_private_key;
-      auto sig_pub_key = sig_key.get_public_key();
-      out << "producer-name = " << name << "\n";
+
+      const auto sig_key = bpsign_private_key;
+      const auto sig_pub_key = sig_key.get_public_key();
+
+      out << "producer-name = " << name_str << "\n";
       out << "private-key = [\"" << string(sig_pub_key) << "\",\"" << string(sig_key) << "\"]\n";
 
-      producer_tuple tp;
-      tp.bpkey = sig_pub_key;
-      tp.name = string_to_name(name.c_str());
-      tp.commission_rate = i == 0 ? 0 : i == 22 ? 10000 : 10000 / i;
-      gs.initial_producer_list.push_back(tp);
+      gs.initial_producer_list.push_back( producer_tuple {
+         bpname, sig_pub_key, 100, std::string("https://www.eosforce.io/") });
+
       tu.key = sig_pub_key;
-      tu.name = string_to_name((std::string(name + 'a')).c_str());
+      tu.name = string_to_name( (name_str + 'a').c_str() );
       gs.initial_account_list.push_back(tu);
-      my_keymap.keymap[string_to_name(name.c_str())] = key;
-      my_keymap.keymap[tu.name] = sig_key;
-      my_sign_keymap.keymap[string_to_name(name.c_str())] = sig_key;
+
+      my_keymap.keymap[bpname]      = key;
+      my_keymap.keymap[tu.name]   = sig_key;
+      my_sign_keymap.keymap[bpname] = sig_key;
    }
 
    // use fix key EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
    const auto private_key = fc::crypto::private_key(std::string("5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"));
 
    my_keymap.add_init_acc(gs, N(eosforce), 10000*10000, private_key);
-   my_keymap.add_init_acc(gs, N(devfund), 1, private_key);
-   my_keymap.add_init_acc(gs, N(eosfund1), 1, private_key);
-   my_keymap.add_init_acc(gs, N(b1), 1, private_key);
+   my_keymap.add_init_acc(gs, N(devfund),            1, private_key);
+   my_keymap.add_init_acc(gs, N(eosfund1),           1, private_key);
+   my_keymap.add_init_acc(gs, N(b1),                 1, private_key);
 
    my_keymap.add_init_acc(gs, N(eosio.msig), 1000*10000, private_key);
 
    // for test
-   my_keymap.add_init_acc(gs, N(force.test), 1*10000, private_key);
-   my_keymap.add_init_acc(gs, N(force.ram), 1*10000, private_key);
-   my_keymap.add_init_acc(gs, N(force.cpu), 1*10000, private_key);
-   my_keymap.add_init_acc(gs, N(force.net), 1*10000, private_key);
-   my_keymap.add_init_acc(gs, N(force.config), 1*10000, private_key);
+   my_keymap.add_init_acc(gs, N(force.test),     1*10000, private_key);
+   my_keymap.add_init_acc(gs, N(force.ram),      1*10000, private_key);
+   my_keymap.add_init_acc(gs, N(force.cpu),      1*10000, private_key);
+   my_keymap.add_init_acc(gs, N(force.net),      1*10000, private_key);
+   my_keymap.add_init_acc(gs, N(force.config), 100*10000, private_key);
 
    // for relay
-   my_keymap.add_init_acc(gs, N(r.token.in), 100*10000, private_key);
-   my_keymap.add_init_acc(gs, N(r.token.out), 100*10000, private_key);
-   my_keymap.add_init_acc(gs, N(r.acc.map), 100*10000, private_key);
+   my_keymap.add_init_acc(gs, N(r.token.in),   100*10000, private_key);
+   my_keymap.add_init_acc(gs, N(r.token.out),  100*10000, private_key);
+   my_keymap.add_init_acc(gs, N(r.acc.map),    100*10000, private_key);
    my_keymap.add_init_acc(gs, N(r.t.exchange), 100*10000, private_key);
 
-   my_keymap.add_init_acc(gs, N(test), 1000*10000, private_key);
+   my_keymap.add_init_acc(gs, N(test), 10000*10000, private_key);
 
    // test accounts
    for( int j = 0; j < 26; ++j ) {
@@ -107,14 +113,24 @@ int main( int argc, const char **argv ) {
       my_keymap.add_init_acc(
             gs,
             string_to_name(n.c_str()),
-            100000, private_key);
+            100000, private_key );
+   }
+
+   for( int j = 0; j < 26; ++j ) {
+      std::string n = "t.";
+      const char mark = static_cast<char>('a' + j);
+      n += mark;
+      my_keymap.add_init_acc(
+            gs,
+            string_to_name(n.c_str()),
+            100000, private_key );
    }
 
    out.close();
 
-   const std::string keypath = "./key.json";
+   const std::string keypath    = "./key.json";
    const std::string sigkeypath = "./sigkey.json";
-   const std::string configini = "./config.ini";
+   const std::string configini  = "./config.ini";
 
    fc::json::save_to_file<eosio::chain::genesis_state>(gs, path, true);
    fc::json::save_to_file<std::vector<account_tuple>>(gs.initial_account_list, activeacc, true);
